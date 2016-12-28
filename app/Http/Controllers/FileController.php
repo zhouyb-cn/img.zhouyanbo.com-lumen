6cf6102b4fc8bb836d19d5d126b30104c5e193d0
yanbo <yanbo@frogshealth.com>
2016-12-28 17:00:00 +0800

    first commit

---
+++ app/Http/Controllers/FileController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class FileController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function test()
+    {
+        return 111;
+    }
+}
